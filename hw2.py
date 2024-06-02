24a429575c1251fd81ee0c382b06ca2537deefce
eeriez <112532572+eeriez@users.noreply.github.com>
2024-06-02 20:55:06 +0300

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,6 @@
+homeworks_completed = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent/homeworks_completed
+
+print('Курс:', course_name, ', всего задач:', homeworks_completed, ', затрачено часов:', hours_spent, ', среднее время выполнения', time_per_task, '.')
