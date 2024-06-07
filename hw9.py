854154ac964f778869451925543d0c9b69c3ddb9
eeriez <112532572+eeriez@users.noreply.github.com>
2024-06-07 19:40:37 +0300

    Update hw9.py
    fixed if clauses so that zeros don't get printed

--- hw9.py
+++ hw9.py
@@ -2,8 +2,11 @@ list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 n = 0
 while True:
-    if list[n] >= 0:
+    if list[n] == 0:
+        n += 1
+        continue
+    elif list[n] > 0:
         print(list[n])
-    else:
+    elif list[n] < 0:
         break
     n += 1
