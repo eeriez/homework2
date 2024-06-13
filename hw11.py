9d1a9c86517f0354730b1faec2b89a4b352109f6
eeriez <112532572+eeriez@users.noreply.github.com>
2024-06-13 14:39:21 +0300

    Add files via upload

---
+++ hw11.py
@@ -0,0 +1,23 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+
+matrix = get_matrix(5, 5, 'a')
+print(matrix)
+
+#Сначала не так поняла задание, написала функцию, которая только выводит в консоль что-то похожее на матрицу.
+#Удалять было жалко, пусть лежит уже
+def print_matrix(n, m, value):
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+        matrix.append(row)
+        print(row)
+
+print_matrix(3, 6, 'ooo')
