4d0d8556d09877159e347f763dae83bed36fcca6
eeriez <112532572+eeriez@users.noreply.github.com>
2024-09-19 16:33:01 +0300

    Update 8_1.py

--- 8_1.py
+++ 8_1.py
@@ -7,6 +7,7 @@ def add_everything_up(a, b):
     else:
         return result.__round__(3)
 
+
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
