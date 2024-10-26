3d585e66c033f5de20eccc4a359be96e5ec47005
eeriez <112532572+eeriez@users.noreply.github.com>
2024-10-26 08:17:42 +0300

    Add files via upload

---
+++ 16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def welcome() -> dict:
+    return {'message': 'Главная страница'}
+
+
+@app.get('/user/admin')
+async def admin() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+
+@app.get('/user/{user_id}')
+async def user(user_id: str) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+
+@app.get('/user')
+async def user(username: str = 'ya', age: int = 20) -> dict:
+    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
