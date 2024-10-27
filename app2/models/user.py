b7ca4a9d5d7c565703fff6edbfddf246168db0f7
eeriez <112532572+eeriez@users.noreply.github.com>
2024-10-27 06:17:12 +0300

    Add files via upload

---
+++ app2/models/user.py
@@ -0,0 +1,14 @@
+from app2.backend.db import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import relationship
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship('Task', back_populates='user')
